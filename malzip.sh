7d7f8cb5a0fc5803a9a899d8f831382faa3d50dc
WizzoTheOrcLovingMisfit <64552696+WizzoTheOrcLovingMisfit@users.noreply.github.com>
2022-04-07 21:01:25 -0400

    Update malzip.sh

--- malzip.sh
+++ malzip.sh
@@ -5,7 +5,7 @@ if [ "" = "$PKG_OK" ]; then
   echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
   sudo apt-get --yes install $REQUIRED_PKG
 fi
-torsocks wget -i $1 --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"
-zip -r -P infected ~/Desktop/mal.zip  $(find . -type f -cmin -1 ! -name "mal.zip" ! -name "malzip.sh" ! -name $1)
-rm $(find . -type f -cmin -1 ! -name "mal.zip" ! -name "malzip.sh" ! -name $1) 
+torsocks wget -P /tmp/malzip/ -i $1 --user-agent="Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion" 
+zip  -m -j -P infected ~/Desktop/$(date +%s)mal.zip  $(find /tmp/malzip/ -type f -cmin -1 ! -name "mal.zip" ! -name "malzip.sh" ! -name $1 )
+rm -rf /tmp/malzip/
 echo "Finished downloading files in "$1
