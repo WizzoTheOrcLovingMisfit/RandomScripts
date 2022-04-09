00742c874cb12dca26732e296c09d0cebfaedfea
WizzoTheOrcLovingMisfit <64552696+WizzoTheOrcLovingMisfit@users.noreply.github.com>
2022-04-09 15:31:44 -0400

    Update malzip.sh
    Fixed User agent

--- malzip.sh
+++ malzip.sh
@@ -5,7 +5,7 @@ if [ "" = "$PKG_OK" ]; then
   echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
   sudo apt-get --yes install $REQUIRED_PKG
 fi
-torsocks wget -P /tmp/malzip/ -i $1 --user-agent="Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion" 
+torsocks wget -P /tmp/malzip/ -i $1 --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0" 
 zip  -m -j -P infected ~/Desktop/$(date +%s)mal.zip  $(find /tmp/malzip/ -type f -cmin -1 ! -name "mal.zip" ! -name "malzip.sh" ! -name $1 )
 rm -rf /tmp/malzip/
 echo "Finished downloading files in "$1
